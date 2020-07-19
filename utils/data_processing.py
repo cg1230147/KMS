cd15660c2ea9888c49aa200715cb9d1ea41f5604
cg1230147 <gcg1230147@163.com>
2020-07-19 21:03:13 +0800

    删除无用注释

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -34,25 +34,3 @@ class Processing(object):
             return self.group_data_join(i['superior_dept'])
         return self.deptName
 
-'''
-json = {'id': 18, 'deptLevel__department_level': '顶级部门', 'superiorDept': None, 'name': '美络科技', 'nodes': [
-    {'id': 19, 'deptLevel__department_level': '一级部门', 'superiorDept': 18, 'name': '北京美络', 'nodes': [
-        {'id': 21, 'deptLevel__department_level': '二级部门', 'superiorDept': 19, 'name': '行政中心', 'nodes': [
-            {'id': 22, 'deptLevel__department_level': '三级部门', 'superiorDept': 21, 'name': '人资部', 'nodes': []},
-            {'id': 23, 'deptLevel__department_level': '三级部门', 'superiorDept': 21, 'name': '行政部', 'nodes': []}
-        ]}
-    ]},
-    {'id': 20, 'deptLevel__department_level': '一级部门', 'superiorDept': 18, 'name': '广州美络', 'nodes': [
-        {'id': 24, 'deptLevel__department_level': '二级部门', 'superiorDept': 20, 'name': '系统开发部', 'nodes': [
-            {'id': 26, 'deptLevel__department_level': '三级部门', 'superiorDept': 24, 'name': 'OA项目部', 'nodes': []}
-        ]},
-        {'id': 25, 'deptLevel__department_level': '二级部门', 'superiorDept': 20, 'name': '移动开发中心', 'nodes': [
-            {'id': 27, 'deptLevel__department_level': '三级部门', 'superiorDept': 25, 'name': 'Android组', 'nodes': []}
-        ]},
-        {'id': 28, 'deptLevel__department_level': '二级部门', 'superiorDept': 20, 'name': '行政中心', 'nodes': [
-            {'id': 29, 'deptLevel__department_level': '三级部门', 'superiorDept': 28, 'name': '人资部', 'nodes': []}
-        ]}
-    ]}
-]}
-'''
-
